8c1e14a889f9b6a7ce1044e1ec7114412008912a
2side <69338087+2side@users.noreply.github.com>
2020-08-07 11:04:13 +0530

    Add files via upload

---
+++ def.js
@@ -0,0 +1,45 @@
+var def={
+    "Rate Of Reaction":"it is the change in concentration of reactant or product per unit time.For a general reaction\n<br><h1>A ------> B</h1><br>RATE OF REACTION=Amount of A consumed/Time Taken\n<br>RATE OF REACTION=Amount of B produced/Time Taken\n\n<br><br>If [A] and [B] are the concentrations of A and B respectively then,\nRATE OF REACTION=∆[A]/∆t=-∆[B]/∆t\n<br>It should be noted here that the rate of reaction is always positive; Negative sign shows that the concentration of the reactant is decreasing and positive sign shows that the concentration of the product is increasing.",
+    
+  "Avg vs Instantaneous Rate": "<img src='img/avg_ins.jpg'>",
+  "Factor Affecting ROR":"<img src='img/f_a_ror.jpg'>",
+  "Catalyst":"It is the substance that alters the rate of a chemical reaction without being used in the reaction and can be recovered chemically unchanged at the end of the reaction.",
+  "Chemical Reaction":"It leads to the transformation of matter into new substances during a chemical change.",
+  "Molar Concentration":"The concentration of solution expressed in terms of moles of solute/volume of solution in liters.",
+  "Product":"It refers to the new substance that is produced as a result of chemical reaction.",
+  "Reactant":"It is a substance that participates in a chemical reaction at the start of the reaction.",
+  "Intermediate Complex Theory":"According to this theory the reactant first combines with catalyst to form a short lived intermediate complex, which eventually decomposes to give product and regenerating catalyst.<br><img src='img/Intermediate Complex Theory.jpg'>",
+  "Threshold Energy":"Threshold energy is the minimum energy that a colliding molecule should have so as to make the collision effective.",
+  "Activation Energy":"It is the minimum extra amount of energy absorbed by the reactant molecule so that its energy becomes equal to the threshold value.",
+  "Collision Theory":`
+  According to this theory, the reactant molecules are assumed to be hard spheres and reaction occurs when molecules collide each other.
+<br>
+<b>Collision Frequency: </b>- The number of collisions per second per unit volume of the reaction mixture is known as collision frequency (Z).
+<h2>
+  Limitations of Collision Theory:-</h2>
+<ol><li>
+It is based on the assumption that atoms or molecules are hard spheres and it ignores their structural aspect.
+</li><li>
+This theory did not explain the high rates of chain reaction.
+ </li> <ol>
+  
+  `,
+  "Rate Law Expression":"<img src='img/Rate Law Expression.jpg'>",
+  "Order Of A Reaction (OOR)":"It is the sum of the exponents to which the concentration terms are raised in the rate law expression.<br>Order of a reaction can be 0, 1, 2 and even a fraction.<br><ol><li><b>Zero Order Reaction:</b> - In these reactions the rate of reaction does not depend upon the concentration of reactants.</li><li><b>First Order Reaction:</b> - Rate of first order reaction depends only on one concentration term.</li><li><b>Second Order Reactions:</b> - Sum of the exponents to which the concentration terms are raised in the rate law expression is two.</li><li><b>Fractional Order Reaction:</b> -Sum of the exponents to which the concentration terms are raised in the rate law expression is a fraction.</li></ol>",
+  "Unit Of Rate Constant (k)":"The units of rate constant depend upon the order of reaction.<br><img src='img/Unit Of Rate Constant (k).jpg'>",
+  "Half Life Of A Reaction":"It is defined as the time in which the concentration of a reactant is reduced to half of its initial concentration. It is represented as t1/2.<ol><li><b>For Zero Order Reaction:- </b>Half life of a zero order reaction is directly proportional to the initial concentration of the reactant and inversely proportional to the rate constant.</li><li><b>For First Order Reaction: -</b>The half-life period for first order reaction is independent of concentration.<br>=> 0.693/k</li><ol>",
+  "Pseudo First Order Reaction":"Those reactions, which are not in actual sense first order but under, certain conditions they become first order.<br><br>For example: Acid catalysed inversion of cane sugar<br><br>Experimentally the reaction is found to be a first order reaction because the rate of reaction is directly proportional to concentration of sucrose only.",
+  "Molecularly Of A Reaction":`
+The number of atoms, ions or molecules that must collide with one another simultaneously resulting into a chemical reaction is called the “Molecularity of a reaction”.
+<ol><li><b>
+Elementary Reactions: </b>A chemical reaction in which one or more chemical species react directly to form products in a single reaction step and with a single transition state is called elementary reaction. Molecularity of a simple reaction is the sum of the molecules of different reactants in a balanced chemical reaction.</li>
+<li><b>
+Complex Reactions: </b>These reactions take place in several steps. Each step of it is termed as the elementary reaction. The several steps in which the complex reaction takes place is called the mechanism of the reaction. </li>`,
+"Arrhenius Equation":"The effect of temperature on the rate of a reaction and hence on the rate constant k, was put forwarded by Arrhenius (1889).<br><img src='https://cdn1.byjus.com/wp-content/uploads/2019/10/Arrhenius-Equation.png'><br>ln k = ln(Ae<sup>-Ea/RT</sup>)<br>ln k = ln(A) + ln(e<sup>-Ea/RT</sup>)<br>",
+"Temperature Coefficient":`Temperature coefficient means that the ratio of the rate of the reaction which is stated at a constant temperature 308K to the rate constant at the temperature 298K.
+<br><br>
+We can hence formulate the above definition into the equation such as. Temperature coefficient = Rate constant at 308/Rate constant at 298K.
+<br><br>
+The value of the rate constant is the variable of temperature rise of 10 degree which can be formulated by K<sub>t+10C</sub>/K<sub>t</sub>.`
+}
+
