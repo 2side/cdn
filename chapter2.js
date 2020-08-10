ea35dc0edc897e96ec368293bf4f2cac521075f2
2side <69338087+2side@users.noreply.github.com>
2020-08-10 11:11:57 +0530

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,63 @@
+var solutions={
+   "Solution &amp; Types":{
+      "Solutions":"They are the homogeneous mixtures of two or more than two components. The component that is present in large quantity is called solvent and the one present in small quantity is called solute. Solvent determines the physical state in which the solution exists.<table border='4'><tr><th>Solute</th><th>Solvent</th><th>Types Of Solutions</th></tr><tr><td>Solid</td><td>Solid</td><td>Solid In Solid</td></tr><tr><td>Liquid</td><td>Solid</td><td>Liquid In Solid</td></tr><tr><td>Gas</td><td>Solid</td><td>Gas In Solid</td></tr><tr><td>Solid</td><td>Liquid</td><td>Solid In Liquid</td></tr><tr><td>Liquid</td><td>Liquid</td><td>Liquid In Liquid</td></tr><tr><td>Gas</td><td>Liquid</td><td>Gas In Liquid</td></tr><tr><td>Solid</td><td>Gas</td><td>Solid In Gad</td></tr><tr><td>Liquid</td><td>Gas</td><td>Liquid In Gas</td></tr><tr><td>Gas</td><td>Gas</td><td>Gas In Gas</td></tr></table>",
+      "Ideal Solutions":"The solutions, which obey Raoult’s law at all temperatures and concentrations, are known as ideal solution.<br /><br>For binary solution AB, ideal behavior is due to the fact that intermolecular interaction between the A-A and B-B are almost equal to those of A-B. <br><br><h3>Characteristic of an Ideal solution</h3><ol><li>Solution and its components obey Raoult's law.</li><li>The change in volume on mixing is zero,<br /> i.e.,∆V<sub>mixing</sub> = 0.</li><li>The change in enthalpy on mixing is zero,<br /> i.e., ∆V<sub>mixing</sub> = 0.</li></ol>",
+      "Non-Ideal Solutions":"These solutions do not obey <abbr>Raoult's law</abbr> over entire range of concentrations.<br>Two liquids A and B form non–ideal solution when the forces of attraction between the molecules of A and B are different from those between A–A or B–B molecules.<br><br><h1>They are of two types:</h1><ol><li><h2>positive deviation from Raoult’s law:</h2> In these solutions, the molecular interactions between A-B are weaker than those between A-A or B-B. As a result molecule of A or B will find it easier to escape in pure state. This will increase the vapour pressure. E.g., methanol and water.For these solutions:<br><ul><li>∆<sub>mix</sub>H&gt;0 or ∆<sub>mix</sub>H is positive</li><li>∆<sub>mix</sub>V&gt;0 or ∆<sub>mix</sub>V is positive</li><li>P<sub>total</sub>&gt;P⁰<sub>AχA</sub>+P⁰<sub>BχB</sub></li></ul></li><li><h2>negative deviation from Raoult’s law:</h2>In these solutions, the molecular interactions between A-B are stronger than those between A-A or B-B. As a result molecule of A or B will find difficulty in escaping from pure state. This will decrease the vapour pressure. E.g., chloroform and acetone<br>For these  solutions:<ul><li>∆<sub>mix</sub>H&lt;0 or ∆<sub>mix</sub>H is negetive</li><li>∆<sub>mix</sub>V&lt;0 or ∆<sub>mix<sub>V is negetive</li><li>P<sub>total</sub>&lt;P⁰<sub>AχA<sub>+P⁰<sub>BχB</sub></li></ul></li></ol>",
+      "Azeotrops":"Some binary liquid mixture has the same composition in liquid and vapour phase and boil at a constant temperature. In such a case it is not possible to separate the components by fractional distillation. They are called as azeotropes.<br><br>They are of two types - Minimum boiling azeotropes and maximum boiling azeotropes.<ul><li>Minimum boiling azeotropes </li><li>show positive deviation from Raoult’s law.</li><li>E.g., a mixture of ethanol and water.</li><li>Maximum boiling azeotropes show negative deviation from Raoult’s law.</li><li>E.g., a mixture of nitric acid and water.</li></ul>"
+   },
+   "Solubility":{
+      "Solubility Of Solid In liquid":"Solubility of solid in liquid depends on the nature of the solvent, nature of the solute, temperature. Pressure does not have significant effect on the solubility of solids in liquids.",
+      "Solubility Of Gas In Liquid":"It is defined as the volume of the gas (in cc.) that can dissolve in unit volume of the liquid to form the saturated solution at a given temperature. It depends on the nature of the gas and solvent, temperature and pressure. It increases with the increase in pressure and decreases with the increase in temperature",
+      "Henry's Law":"According to this law, the partial pressure of gas in vapour phase (p) is proportional to the mole fraction of gas (χ) in the solution.<br>P=K<sub>H</sub>χ<br>Where, χ = mole fraction of gas<br>KH is the Henry’s constant."
+   },
+   "Colligative Properties":{
+      "Relative Lowering Of Vapour pressure":"When a non-volatile solute is added to a solvent, the vapour pressure of the solution is lower than that of the pure solvent.<br>The relative lowering in vapour pressure of an ideal solution is equal to the mole fraction of the solute at a given temperature.<br>P=P⁰χ<sub>solvent</sub><br>P=P⁰(1-χ<sub>solute</sub>)<br><sup>P</sup>/<sub>p⁰</sub>=1-χ<sub>solute</sub><br><sup>P⁰-P</sup>/<sub>P⁰</sub>=χ<sub>solute</sub>",
+      "Elevation In Boiling Point":"Boiling point of a liquid is defined as the temperature at which vapour pressure of the liquid becomes equal to the atmospheric pressure. When a solute is added to the solvent, the boiling point of solution increases as compared to the pure solvent.<br><b>∆T<sub>b</sub>=K<sub>b</sub>m</b><br>∆T<sub>b</sub>=Elivation in boiling point<br>K<sub>b</sub>=molal elevation constent<br>m = molality",
+      "Depression In Freezing Point":"Freezing point of a liquid is defined as the temperature at which the vapour pressure of the liquid and its solid phase becomes equal.<br />When solute is added to the solvent, there is a depression in the freezing point of the solution.<br><b>∆T<sub>f</sub>=K<sub>f</sub>m</b><br>∆T<sub>f</sub>=depression in freezing point<br>K<sub>f</sub>=molal depression constent<br>m = molality",
+      "Osmotic Pressure":"Osmosis is the movement of solvent molecules from the region of their higher concentration to the lower concentration (or pure solvent to solution) through a semi permeable membrane. Semi permeable membrane is made of cellophane paper or pig’s bladder or parchment. The movement of solvent molecules continues till the equilibrium is attained.<br />Osmotic pressure is the pressure exerted by the flow of water through a semi permeable membrane separating two solutions with different concentrations of solute.<br><b>π=CRT</b><br>On the basis of their osmotic pressure, solutions can be:<br><b>1. Isotonic solutions: -</b> Those solutions, which have the same osmotic pressure at the same temperature, are known as isotonic solutions. Example: - A 0.9% solution of pure NaCl is isotonic with human red blood cells.<br><b>2.Hypertonic solution:-</b> A solution having higher osmotic pressure (higher concentration) is said to be hypertonic with respect to the solution having lower osmotic pressure.<br><b>Example:</b> - if we place the red blood cell in a solution containing more than 0.9% NaCl, water will flow out of the cells and they would shrink. This phenomenon is known as plasmolysis.<br><b>3.Hypotonic solution:-</b> A solution having lower osmotic pressure (lower concentration) is said to be hypotonic with respect to the one having higher osmotic pressure.<br><b>Example:- </b>If we place the blood cells in a solution containing less than 0.9% NaCl, water will flow into the cells and they would swell and eventually burst. This phenomenon is known as hemolysis.",
+      "Application Of Osmotic pressure":"<ol><li>Raw mango placed in concentrated salt solution (salt & spices) loses water via osmosis and shrivel into pickle.</li><li>Wilted flowers become fresh again when placed in fresh water</li><li>A carrot/reddish that has become limp because of water loss into the atmosphere becomes fresh again when placed in water for sometime</li><li>People taking a lot of salt or salty food experience water retention in their cells and intercellular spaces because of osmosis. The resulting puffiness or swelling is called edema</li><li>Meat is preserved by adding salt and fruits are preserved by adding sugar. Through the process of osmosis, a bacterium on salted meat or candid fruit loses water, shrivels and dies</li><li><b>Reverse osmosis: -</b>In this process, pressure larger than osmotic pressure is applied on the solution side. As a result pure solvent flows out of the solution through semi permeable membrane. It is used in desalination of seawater</li></ol>"
+   },
+   "Abnormal Molar Mass":{
+   
+      "Examples":"<b>1)</b>Each KCl molecule dissociates to give two ions (K<sup>+</sup> & Cl<sup>-</sup>). Thus the number of particle becomes double. As a result the observed molar mass is about half of the expected value.<br><b>2)</b>When acetic acid is dissolved in benzene (Non-aqueous solvent) it exists as dimmer due to hydrogen bonding between two molecules. Thus, the number of particles in the solution decreases. As a result the observed molar mass of acetic acid is higher than that of theoretically expected value.",
+      "Von't Hoff Factor":`introduced a factor i (van’t Hoff factor), to account for the extent of dissociation or association.<br><br>van’t Hoff factor is the ratio of the observed (experimental) value of a colligative property to the normal (calculated) value of the same property.<pre><span style='border-bottom:2px solid'>experimental colligative property</span>
+calculated colligative property
+      </pre>`,
+      "Modified expressions for colligative properties":"Relative Lowering Of Vapour pressure -:<b><sup>P⁰-P</sup>/<sub>P⁰</sub>=iχ<sub>solute</sub></b><br>Osmotic pressure -:  <b>π=iCRT</b><br>Elevation In BP -: <b>∆T<sub>b</sub>=K<sub>b</sub>m</b><br>Depression In F.P -:  <b>∆T<sub>f</sub>=K<sub>f</sub>m</b>"
+   },
+   "Glossary":{
+      "Concentration Term":"The relative amount of solute in binary solution can be expressed by any of the following terms: Mass percentage, volume percentage, parts per million (ppm), Molarity (M), Molality (m), Normality (n) and Mole fraction."+` <table border='2'>
+        <tr>
+            <th>Molarity(M)</th><td><pre><span style='border-bottom:3px solid'>      moles of solute       </span>
+Volume Of Solutions(in ltr)</pre></td>
+        </tr>
+         <tr>
+            <th>Molality(m)</th><td><pre><span style='border-bottom:3px solid'>      moles of solute     </span>
+Weight of solvent(in kg)</pre></td>
+        </tr>
+         <tr>
+            <th>Normality(N)</th><td><pre><span style='border-bottom:3px solid'>  gram equivalents of solute</span>
+Volume Of Solutions(in ltr)</pre></td>
+        </tr>
+         <tr>
+            <th>Formality(f)</th><td><pre><span style='border-bottom:3px solid'>        Weight in gram     </span>
+Formula wt×vol of sol(in ltr)</pre></td>
+        </tr>
+         <tr>
+            <th>Mole Fraction(X<sub>A</sub>)</th><td><pre><span style='border-bottom:3px solid'>   n<sub>A</sub>   </span>
+  n<sub>A</sub>+n<sub>B</sub></pre></td>
+        </tr>
+         <tr>
+            <td>Parts Per million(ppm)</td><td><pre><span style='border-bottom:3px solid'>mass of solute </span>× 10<sup>6</sup>
+mass of solvent</pre></td>
+        </tr>
+    </table>`,
+      "Raoult's Law":"It gives quantitative relationship between vapour pressure and mole fraction. It states that for a solution of volatile liquids, the partial pressure of each component in the solution is directly proportional to its mole fraction.<br>or a solution containing two components A & B,<br>For component A<br>P<sub>a</sub>¢χ<sub>a</sub><br>P<sub>a</sub>=P⁰<sub>a</sub>χ<sub>a</sub><br>For component B<br>P<sub>b</sub>¢χ<sub>b</sub><br>P<sub>b</sub>=P⁰<sub>b</sub>χ<sub>b</sub>",
+      "Amalgam":"It is an alloy of mercury with another metal or metals except Pt and Fe.",
+      "Binary Solution":"It is a solution consisting of two components",
+      "Dimer":"It is the aggregate formed when two molecules of a solute combine together",
+      "Non-Volatile Solute":"It is a solute which does not vapourise readily.",
+      "Partial Pressure":"It is the pressure exerted by an individual gas in a mixture of gasses",
+      "Scuba Divers":"Scuba is a portable breathing device for free-swimming divers. It consists of a mouthpiece joined by hoses to one or two tanks of compressed air that are strapped on the back"
+   }
+}  
