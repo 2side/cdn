0655e2552e74ed98ec0b928e58906f0d1afa8423
2side <69338087+2side@users.noreply.github.com>
2020-08-11 12:20:54 +0530

    Add files via upload

---
+++ qq.js
@@ -0,0 +1,104 @@
+var qq={
+    "programming":[{que:"which of these Elements are all <table> Elements",option:["<table><tr><td>","<table><tr><tt>","<table><foot><head>","<table><tb><tt>"],crt:0},{que:"How do you define a custom property in css?",option:["$custom-property = 10","$custom-property : 10","--custom-property : 10","--custom-property = 10"],crt:2},{que:"in CSS, what is the order of the box model from the inside out",option:["Content, Margin, Border, Padding","Padding, Margin, Border, Content","Content, Padding, Border, Margin","Border, Padding, Margin, Content"],crt:2},{que:"How do you check grid support in css",option:["@supports(grid)","@supports(display:grid)","@feature(display:grid)","@feature(grid)"],crt:1},{que:"Which of the following is not a valid CSS property",option:["text-wrap","will-change","empty-cells","border-image"],crt:0},{que:"How do you define multiple transition in css?",option:["transition: width 1s, height 2s","transition: width  height, 1s 2s","transition: width 1s/ height 2s","transition: width, 1s, height, 2s"],crt:0},{que:"What is the result of 100/0 in JavaScript",option:["it throws an error","0","infinity","NaN"],crt:2},{que:"in Node.js, how do you import a module using CommonJs.",option:["const a = require('f')","const a = import('f')","const a from ('f')","const a = include('f')"],crt:0},{que:"which of the following false",option:["' ' == false","false === false","null == undefined","NaN == NaN"],crt:3},{que:"ReactJS uses _____ to increase performance",option:["Original DOM","Virtual DOM","Shadow DOM","Web DOM"],crt:1},{que:"Key in React Js, must be unique in which scope?",option:["Globally","inside Component","in the loop they are used in","in the component they are rendering in"],crt:2},{que:"What year was React Js open-sourced?",option:["2015","2014","2013","2012"],crt:2},{que:"Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",option:["indexOf()","getIndex()","location()","slice()"],crt:0},{que:"Which of the following function of String object is used to match a regular expression against a string?",option:["search","replace()","concat()","match()"],crt:3},{que:"Which of the following function is used to locate a string within a string?",option:["strpos()","search()","locate()","strlen()"],crt:0},{que:"What does PHP stand for?",option:["Preprocessed Hypertext Page","Hypertext Markup Language","PHP: Hypertext Preprocessor","Hypertext Transfer Protocol"],crt:2},{que:"Which of the following is NOT a valid PHP comparison operator",option:["!=",">=","<>","<=>"],crt:3},{que:"Which of the following is NOT a magic predefined constant?",option:["__LINE__","__FILE__","__DATE__","__CLASS__"],crt:2},{que:"In which variable is the users IP address stored?",option:["$DOMAIN","$REMOTE_ADDR","$_ENV","$GLOBALS"],crt:1},{que:"When was the sololearn app launched?",option:["2011","2012","2013","2014"],crt:3}],
+    "solutions":[
+        {
+       "que":"The mole fraction of the solute in one molal aqueous solution is",
+       "option":["0.009","0.018","0.027","0.036"],
+       "crt":1
+   },
+      {
+       "que":"5cm<sup>3</sup> of acetone is added to 100cm<sup>3</sup> of water, the vapour pressure of water over the solution will be",
+       "option":["equal to the vapour pressure of pure water","less than the vapour pressure of pure water","greater than the vapour pressure of pure water","very large"],
+       "crt":1
+   },
+      {
+       "que":"The density of a solution prepared by dissolving 129g of urea in 1000g of water is 1.15g/ml. The molarity of this solution is",
+       "option":["1.78M","1.02M","2.05M","0.50M"],
+       "crt":2
+   }
+   ,   {
+       "que":"If at certain temperature the vapour pressure of pure water is 25mm Hg and that of a very dilute aqueous urea solution is 24.5mm Hg, the molality of the solution is:",
+       "option":["0.02","1.2","1.11","0.08"],
+       "crt":2
+   }
+   ,   {
+       "que":"The relationship between osmotic pressure at 273K when 10g glucose (P1), 10g urea (P2) and 10g sucrose (P3) are dissolved in 250ml of water is",
+       "option":["P1 > P2 > P3","P3 > P1 > P2","P2 > P1 > P3","P2 > P3 > P1"],
+       "crt":2
+   },   {
+       "que":"Two liquids X and Y form an ideal solution. At 300K vapour pressure of the solution containing 1mol of X and 3 mol of Y is 550 mm Hg. At the same temperature, if 1 mol of Y is further added to this solution, vapour pressure of the solution increases by 10 mm Hg. Vapour pressure of X and Y in the pure states will be respectively.",
+       "option":["300 and 400","400 and 600","500 and 600","200 and 300"],
+       "crt":1
+   },
+      {
+       "que":"Mixing up equal volumes of 0.1M NaOH and 0.1M CH3COOH yields a solution which is",
+       "option":["basic","acidic","neutral","None"],
+       "crt":0
+   }
+   
+   ,   {
+       "que":"Equal volumes of 0.1M AgNO3 and 0.2M NaCl are mixed. The concentration of NO<sub>3</sup>- ions in the mixture will be",
+       "option":["0.1M","0.05M","0.2M","0.15M"],
+       "crt":1
+   }
+    ,
+       {
+       "que":"Which of the following should be done in order to prepare 0.4N NaCl starting with 100ml of 0.3M NaCl?",
+       "option":["add 5.85g of NaCl","add 20ml water","add 0.01 mol NaCl","evaporate 10ml of water"],
+       "crt":2
+   },
+   {
+       "que":"The volume of 0.025M H<sub>3</sub>PO<sub>4</sub> required to neutralise 25ml of 0.03M Ca(OH)<sub>2</sub> is",
+       "option":["20ml","25ml","40ml","50ml"],
+       "crt":0
+   }
+    
+        ],
+        "chemical-kinetics":[
+            {
+       "que":"The rate of a chemical reaction tells us about",
+       "option":["","","",""],
+       "crt":0
+   }  , {
+       "que":"",
+       "option":["the reactants taking part in reaction","the products formed in the reaction","how slow or fast the reaction is taking place","none of the above"],
+       "crt":2
+   }  , {
+       "que":"In the rate equation, when the concentration of reactants is unity then rate is equal to",
+       "option":["specific rate constant","average rate constant","instantaneous rate constant","None of the above"],
+       "crt":0
+   }  , {
+       "que":"The average rate and instantaneous rate of a reaction are equal",
+       "option":["at the start","at the end","in the middle","when two rate have time interval equal to zero"],
+       "crt":3
+   }  , {
+       "que":"What of the following observations is incorrect about the order of a reaction?",
+       "option":["Order can only be assessed experimentally","The stoichiometric coefficient of the reactants doesn’t affect order.","Order of reaction is the sum of power to express the rate of reaction to the concentration terms of the reactants.","The cumulative number is also the order of reaction."],
+       "crt":3
+   } ,  {
+       "que":"There are constant units of first and zero order reactions in terms of M units of molarity",
+       "option":["sec<sup>-1</sup>, Msec<sup>-1</sup>","sec<sup>-1</sup>, M","Msec<sup>-1</sup>, sec-1","M, sec<sup>-1</sup>"],
+       "crt":0
+   }  , {
+       "que":"In the reaction 2A + B → A<sub>2</sub>B if the concentration of A is doubled and that of B is halved, then the rate of the reaction will",
+       "option":["increase 2 times","increase 4 times","decrease 2 times","remain the same"],
+       "crt":0
+   } ,  {
+       "que":"The reactant Vs time concentration diagram for a reaction is a straight line with a negative pendulum. The reaction follows an equation for the intensity.",
+       "option":["zero order","first order","second order","third order"],
+       "crt":0
+   }  , {
+       "que":"A substance ‘A’ decomposes by a first order reaction starting initially with [A] = 2.00M and after 200min, [A] becomes 0.15M. For this reaction t1/2 is",
+       "option":["53.72 min","50.49 min","48.45 min","46.45 min"],
+       "crt":0
+   }  , {
+       "que":"In the Haber process for the manufacture of ammonia the following catalyst is used",
+       "option":["Platinized asbestos","Iron with molybdenum as promoter","Copper oxide","Alumina"],
+       "crt":1
+   } ,  {
+       "que":"In a reversible reaction the energy of activation of the forward reaction is 50 kcal. The energy of activation for the reverse reaction will be",
+       "option":["< 50 kcal","either greater than or less than 50 kcal","= 50 kcal","> 50 kcal"],
+       "crt":1
+   }
+            ]
+}
